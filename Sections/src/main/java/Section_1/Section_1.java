bc94601821a8b9cd22019701cd91abf7b7eab28b
loyd_ <loyd_pintac@yahoo.com>
2022-12-12 10:14:03 -0600

    Adding task

--- Sections/src/main/java/Section_1/Section_1.java
+++ Sections/src/main/java/Section_1/Section_1.java
@@ -2,7 +2,31 @@ package Section_1;
 
 public class Section_1 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        //Add an integer variable and assign your age to it --> Lloyd
+
+
+        //Add an integer variable and assign your age to it --> Josh
+
+
+        //Add a String variable and assign your name to it- Lloyd
+        String lloydName = "Lloyd Pintac";
+
+        //Add a String variable and assign your name to it- Josh
+
+
+        //Print your name - Lloyd
+        System.out.println("My name is "+lloydName);
+
+        //Print your name - Josh
+
+
+        //Print your age - Lloyd
+
+
+        //Print your age - Josh
+
+
+
     }
 }
 
